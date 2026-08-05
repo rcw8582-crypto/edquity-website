eaa4a983c6e1c70916486be82230b09b63759c8e
rcw8582-crypto <rcw8582@gmail.com>
2026-08-05 14:35:27 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,7 @@ const Admin = lazy(() => import("@/pages/Admin"));
 const Intake = lazy(() => import("@/pages/Intake"));
 const ParentQuestions = lazy(() => import("@/pages/ParentQuestions"));
 const Fellowship = lazy(() => import("@/pages/Fellowship"));
+const Book = lazy(() => import("@/pages/Book"));
 const IepQualityImprovement = lazy(() => import("@/pages/IepQualityImprovement"));
 const NotFound = lazy(() => import("@/pages/not-found"));
 
@@ -120,6 +121,7 @@ function Router() {
           <Route path="/intake" component={Intake} />
           <Route path="/tell-us-about-your-child" component={ParentQuestions} />
           <Route path="/fellowship" component={Fellowship} />
+          <Route path="/book" component={Book} />
           <Route path="/iep-quality-improvement" component={IepQualityImprovement} />
           <Route component={NotFound} />
         </Switch>
